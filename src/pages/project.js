5476b8b2e3502ddded9da18f22f4232a491a6591
Kyle Mcleod <kylemcleod1@gmail.com>
2019-06-04 20:06:24 -0700

    setup project framework and import images

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,10 +1,13 @@
-import React from "react"
+import React, { Fragment } from "react"
 import styled from "styled-components"
 import { FiChevronsLeft } from "react-icons/fi"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 import SEO from "../components/seo"
 import Background from "../images/desk1.jpg"
+import tribePilot from "../images/tribepilot.png"
+import pickup from "../images/pickup.png"
+import nscale from "../images/nscale.png"
 
 const Wrapper = styled.div`
   background-image: url(${props => props.src});
@@ -31,11 +34,43 @@ const Column = styled.div`
   width: 50vw;
 `
 
+const Body = styled.div`
+  border: 2px solid white;
+  border-radius: 5px;
+  height: 70vh;
+  width: 45vw;
+  margin-left: 5vw;
+  padding: 20px 50px;
+`
+
+const Image = styled.img`
+  max-width: 40vw;
+  max-height: 40vh;
+`
+
 const Project = props => {
   const { location } = props
+
   if (!location.state) {
     return <h1>Sorry this page doesn't seem to be working</h1>
   }
+
+  let pic
+  switch (location.state.project.name) {
+    case "Tribe Pilot":
+      pic = tribePilot
+      break
+    case "nscale":
+      pic = nscale
+      break
+    case "Pickup":
+      pic = pickup
+      break
+    default:
+      pic = ""
+      break
+  }
+
   return (
     <Wrapper src={Background}>
       <Overlay>
@@ -57,17 +92,24 @@ const Project = props => {
           <FiChevronsLeft size="4rem" />
         </AniLink>
         <Column>
-          <h2 style={{ width: "100%", textAlign: "center" }}>
-            {location.state.project
-              ? location.state.project.name
-              : "Not sure why... But cant retrieve name at this time"}
-          </h2>
+          <Body>
+            <h2 style={{ width: "100%", textAlign: "center" }}>
+              {location.state.project
+                ? location.state.project.name
+                : "Not sure why... But cant retrieve name at this time"}
+            </h2>
+          </Body>
         </Column>
         <Column>
           <h2 style={{ width: "100%", textAlign: "center" }}>
-            {location.state.project
-              ? location.state.project.name
-              : "Not sure why... But cant retrieve name at this time"}
+            {location.state.project ? (
+              <a href="https://www.tribepilot.com" target="_blank">
+                <h2>{location.state.project.name}</h2>
+                <Image src={pic} />
+              </a>
+            ) : (
+              "Not sure why... But cant retrieve name at this time"
+            )}
           </h2>
         </Column>
       </Overlay>
